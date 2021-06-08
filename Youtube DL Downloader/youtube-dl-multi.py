4f97d45da932378513ecd18598034fd625f7b86c
khuang08 <khuang08@gmail.com>
2021-06-07 23:25:28 -0700

    Update youtube-dl-multi.py
    updating %s to f strings

--- Youtube DL Downloader/youtube-dl-multi.py
+++ Youtube DL Downloader/youtube-dl-multi.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from easygui import *
 import tkinter as tk
 from tkinter import messagebox
@@ -6,7 +6,14 @@ from tkinter import messagebox
 if __name__ == "__main__":
 
 	count = 0
-	os.chdir("D:\\youtube-dl")
+
+	path = "C:\\youtube-dl"
+
+	# checks if folder exists
+	if os.path.isdir(path) == False:
+		os.mkdir(path)
+
+	os.chdir(path)
 
 	root = tk.Tk()
 	root.withdraw()
@@ -17,22 +24,30 @@ if __name__ == "__main__":
 	default_list= ['','','','','']
 	
 	url_list = multenterbox(text, title, fieldNames, default_list)
-		
+	
+	# Exit if user presses Cancel
+	if url_list == None:
+		tk.messagebox.showinfo("Youtube-dl", f"Exiting.")
+		sys.exit()
+
+	# remove blank entries in URL list
 	while("" in url_list): 
 		url_list.remove("")
 
+	# go through each URL and download
 	for url in url_list:
 		try:
-			os.system('youtube-dl -f best "%s"' % url)
+			os.system(f"youtube-dl -f best {url}")
 			count += 1
 		except:
-			tk.messagebox.showinfo('Youtube-dl', 'Invalid URL: %s.\n' % url)
+			tk.messagebox.showinfo("Youtube-dl", f"Invalid URL: {url}.\n")
 
 	if count > 0:
 		plural = 's'
 	else:
 		plural = ''
 
-	tk.messagebox.showinfo('Youtube-dl', '%s video%s downloaded. Exiting.\n' % (count,plural))
+	tk.messagebox.showinfo("Youtube-dl", f"{count} video{plural} downloaded to {path}. Exiting.\n")
 
 	root.destroy()
+
